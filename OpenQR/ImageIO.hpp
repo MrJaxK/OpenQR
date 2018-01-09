2f99dc67ca71d545878a6c0360c965d6b1637500
Mr.K <1061494593@qq.com>
2018-01-09 18:39:55 +0800

    Fix bug on macOS

--- OpenQR/ImageIO.hpp
+++ OpenQR/ImageIO.hpp
@@ -17,7 +17,7 @@ namespace openqr
 		template<typename T>
 		Matrix<T> ImageRead(const std::string& filePath);
 		template<typename T>
-		bool ImageSave(const std::string& filePath, Matrix<T>& mat);
+		bool ImageSave(const std::string& filePath, Matrix<T> mat);
 		~ImageIO();
 	private:
 		enum ImageType
@@ -62,7 +62,7 @@ namespace openqr
 		return matImage;
 	}
 	template<typename T>
-	inline bool ImageIO::ImageSave(const std::string & filePath,Matrix<T>& mat)
+	inline bool ImageIO::ImageSave(const std::string & filePath,Matrix<T> mat)
 	{
 		ImageType type = ImageTypeCheck(filePath);
 		IImageIO<T>* imageProcesser = ImageTypeSet<T>(type);
